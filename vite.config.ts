351e85a7f0c0a1497ec85a97b5ff85d52156cc3a
PowerX-NOT <pugalg100@gmail.com>
2025-03-08 20:29:04 +0530

    use GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rush.60/', // Set this to your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
